86bef0e478b0e94a0cb4dd1db005412cc8be2ea3
Paul Rogers <paulwrogers@yahoo.com>
2019-01-26 16:24:55 -0800

    added logic for random card

--- src/App.js
+++ src/App.js
@@ -6,10 +6,49 @@ import cows from "./cows.json"
 
 class App extends Component {
   state = {
-    message: "Click a cow to start!",
-    cows,
+      message: "Click an image to begin!",
+      topScore: 0,
+      curScore: 0,
+      cows: cows,
+      unselectedCows: cows
   }
 
+componentDidMount() {
+}
+
+shuffleArray = array => {
+    for (let i = array.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+}
+selectCow = breed => {
+    const findCow = this.state.unselectedCows.find(item => item.breed === breed);
+
+    if(findCow === undefined) {
+        // failure to select a new dog
+        this.setState({ 
+            message: "You guessed incorrectly!",
+            topScore: (this.state.curScore > this.state.topScore) ? this.state.curScore : this.state.topScore,
+            curScore: 0,
+            cows: cows,
+            unselectedCows: cows
+        });
+    }
+    else {
+        // success to select a new dog
+        const newCows = this.state.unselectedCowss.filter(item => item.breed !== breed);
+        
+        this.setState({ 
+            message: "You guessed correctly!",
+            curScore: this.state.curScore + 1,
+          cows: cows,
+            unselectedCows: newCows
+        });
+    }
+
+    this.shuffleArray(cows);
+};
   render() {
     return (
       <div className="All-cows">
@@ -28,3 +67,4 @@ class App extends Component {
 }
 console.log(CowCard)
 export default App;
+
