cb944fa0193ed7136009d5967d65d53923ea214f
Paul Rogers <paulwrogers@yahoo.com>
2019-01-28 09:35:35 -0800

    Updates

---
+++ precache-manifest.1f17841e02ae73116c3527ee26e3c0ac.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6acb6692e4ee25941da6",
+    "url": "/clicky-game/static/js/runtime~main.6acb6692.js"
+  },
+  {
+    "revision": "de5f7bb2bb5a83be9951",
+    "url": "/clicky-game/static/js/main.de5f7bb2.chunk.js"
+  },
+  {
+    "revision": "9c49b1657f794805e3dd",
+    "url": "/clicky-game/static/js/1.9c49b165.chunk.js"
+  },
+  {
+    "revision": "de5f7bb2bb5a83be9951",
+    "url": "/clicky-game/static/css/main.9f22798f.chunk.css"
+  },
+  {
+    "revision": "7e58852f9237481cf5a9c4f6b2614a55",
+    "url": "/clicky-game/index.html"
+  }
+];
